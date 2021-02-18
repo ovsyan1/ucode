dcd015ea4b9ed15b8c9e655d417ebefa94584a92
Kyrylo <77015424+ovsyan1@users.noreply.github.com>
2021-02-18 10:27:11 +0200

    t04
    t04

---
+++ sprint03/t04/js/script.js
@@ -0,0 +1,59 @@
+
+function concat(string1, string2){
+    if(string1 && string2){
+        return string1.concat(' ' + string2)
+    }else {
+        function func1(){
+            func1.count++;
+            return string1 + ' ' + prompt()
+    } 
+    func1.count = 0
+    return func1
+    }
+    
+ }
+
+
+
+
+
+let phrase1 = concat("Hulk", "smash!");
+let output = phrase1;
+console.log(output); // Hulk smash!
+
+let phrase2 = concat("Leave");
+output= phrase2();
+// a prompt appears. Enter "Hulk alone!" into the prompt
+
+console.log(output); // Leave Hulk alone!
+console.log(phrase2.count); // 1
+output= phrase2();
+// a prompt appears. Enter "me alone, please!" into the prompt
+
+console.log(output); // Leave me alone, please!
+
+output= phrase2();
+// a prompt appears. Enter "HULK ALONE!" into the prompt
+
+console.log(output); // Leave HULK ALONE!
+console.log(phrase2.count); // 3
+
+let phrase3 = concat("Go");
+output= phrase3();
+// a prompt appears. Enter "away!" into the prompt
+
+console.log(output); // Go away!
+console.log(phrase3.count); // 1
+console.log(phrase2.count); // 3
+
+/* Result in Console panel:
+Hulk smash!
+Leave Hulk alone!
+1
+Leave me alone, please!
+Leave HULK ALONE!
+3
+Go away!
+1
+3
+*/
